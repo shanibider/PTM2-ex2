b412b086fe3b53810f3622491b0918948aaea232
shanibider <shanibider@gmail.com>
2021-01-18 18:40:06 +0200

    Main: out test main
    NoneSafeInteger:using for testing (this is not thread safe class!)

---
+++ test/src/test/SafeInteger.java
@@ -0,0 +1,12 @@
+package test;
+
+public interface SafeInteger {
+
+    public SafeInteger increment();
+    public SafeInteger decrement();
+    public SafeInteger add(int add);
+    public SafeInteger pow(int pow);
+    public SafeInteger mul(int mul);
+    public int getValue();
+
+}
